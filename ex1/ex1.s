0f09219befa94e12b8db6f5984640a6940fc1d29
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2014-02-06 23:07:06 +0100

    Moved vectors to other file

--- ex1/ex1.s
+++ ex1/ex1.s
@@ -1,73 +1,7 @@
 .syntax unified
 
 .include "efm32gg.s"
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// Exception vector table
-// This table contains addresses for all exception handlers
-//
-/////////////////////////////////////////////////////////////////////////////
-
-.section .vectors
-
-.long   stack_top               /* Top of Stack                 */
-.long   _reset                  /* Reset Handler                */
-.long   dummy_handler           /* NMI Handler                  */
-.long   dummy_handler           /* Hard Fault Handler           */
-.long   dummy_handler           /* MPU Fault Handler            */
-.long   dummy_handler           /* Bus Fault Handler            */
-.long   dummy_handler           /* Usage Fault Handler          */
-.long   dummy_handler           /* Reserved                     */
-.long   dummy_handler           /* Reserved                     */
-.long   dummy_handler           /* Reserved                     */
-.long   dummy_handler           /* Reserved                     */
-.long   dummy_handler           /* SVCall Handler               */
-.long   dummy_handler           /* Debug Monitor Handler        */
-.long   dummy_handler           /* Reserved                     */
-.long   dummy_handler           /* PendSV Handler               */
-.long   dummy_handler           /* SysTick Handler              */
-
-/* External Interrupts */
-.long   dummy_handler
-.long   gpio_handler            /* GPIO even handler */
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   gpio_handler            /* GPIO odd handler */
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
-.long   dummy_handler
+.include "vectors.s"
 
 .section .text
 
