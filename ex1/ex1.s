be72d270bf7f27ec17743b59fed12daebb5368d8
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2014-02-07 01:29:56 +0100

    Ye old event done

--- ex1/ex1.s
+++ ex1/ex1.s
@@ -1,7 +1,7 @@
 .syntax unified
 
-.include "efm32gg.s"
-.include "vectors.s"
+.include "lib/efm32gg.s"
+.include "lib/vectors.s"
 
 .section .text
 
@@ -17,84 +17,34 @@
 .thumb_func
 _reset:
 
-cmu_base_addr:
-    .long CMU_BASE
+    // aliases for constants, don't change these
+    gpio .req r9
+    gpio_o .req r8
+    gpio_i .req r7
+    cw .req r6
+    ldr r9, =GPIO_BASE
+    ldr r8, =GPIO_PA_BASE
+    ldr r7, =GPIO_PC_BASE
 
-gpio_pa_base_addr:
-    .long GPIO_PA_BASE
-
-// LOAD CMU BASE ADDRESS
-ldr r1, cmu_base_addr
-
-// load current value of HFPERCLK ENABLE
-ldr r2, [r1, #CMU_HFPERCLKEN0]
-
-// set bit for GPIO clk
-mov r3, #1
-lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
-orr r2, r2, r3
-
-// store new value
-str r2, [r1, #CMU_HFPERCLKEN0]
+    // LOAD CMU BASE ADDRESS
+    ldr r1, =CMU_BASE
+    ldr r2, [r1, #CMU_HFPERCLKEN0]
+    mov r3, #1
+    lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+    orr r2, r2, r3
+    str r2, [r1, #CMU_HFPERCLKEN0]
 
 
-// ---> set high drive strength by writing 0x2 to gpio_pa_ctrl
-// load gpio pa base
-ldr r1, gpio_pa_base_addr
+    // set high drive_strength
+    mov r2, #0x2
+    str r2, [gpio_o, #GPIO_CTRL]
 
-// set bit for GPIO_PA_CTRL
-mov r2, #0x2
+    // set pins to output
+    mov r2, #0x55555555
+    str r2, [gpio_o, #GPIO_MODEH]
 
-// store value
-str r2, [r1, #GPIO_CTRL]
-
-// --->
-// r1 still has gpio_pa_base_addr in it
-// set bit
-mov r2, #0x55555555
-
-// store value
-str r2, [r1, #GPIO_MODEH]
-
-// boot sequence
-mov r2, #0x0100
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x0200
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x0400
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x0800
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x1000
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x2000
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x4000
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x8000
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0xff00
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x0000
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0xff00
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x0000
-str r2, [r1, #GPIO_DOUT]
-
-mov r2, #0x0000
-str r2, [r1, #GPIO_DOUT]
+    // boot sequence
+    bl boot_sequence
 
 // Setup for input
 
@@ -107,11 +57,82 @@ str r2, [r1, #GPIO_DOUT]
 
 .thumb_func
 gpio_handler:
-b .  // do nothing
+    b .  // do nothing
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// Other
+// Other functions as the result of procrastination
+//
+/////////////////////////////////////////////////////////////////////////////
+
+.thumb_func
+casual_wait:
+    mov cw, #0x00a0000
+cwait_loop:
+    subs cw, #1
+    bne cwait_loop
+    bx lr
+
+.thumb_func
+start_seq:
+    push {lr}
+
+    bl high_rise
+
+    // high rise
+high_rise:
+    ror r1, r2
+    str r1, [gpio_o, #GPIO_DOUT]
+    bl casual_wait
+    subs r3, #1
+    bne high_rise
+
+    // reset constants
+    mov r3, #8      // see line below with the same constant
+    bl low_cooling
+
+    // low cooling
+low_cooling:
+    lsl r1, r2
+    str r1, [gpio_o, #GPIO_DOUT]
+    bl casual_wait
+    subs r3, #1
+    bne low_cooling
+
+    // reset constants prepare for ye olde big effect
+    mov r3, #4
+    bl ye_olde_big_effect
+
+ye_olde_big_effect:
+    and r4, r1, #0xf000
+    and r5, r1, #0x0f00
+    lsl r4, r4, #1
+    lsr r5, r5, #1
+    orr r1, r4, r5
+    and r1, r1, #0xff00
+    str r1, [gpio_o, #GPIO_DOUT]
+    bl casual_wait
+    subs r3, #1
+    bne ye_olde_big_effect
+
+    pop {pc}
+
+.thumb_func
+boot_sequence:
+    push {lr}
+
+    mov r1, #0xff00 // start position for lights
+    mov r2, #1      // positions rotated
+    mov r3, #8      // jumps made by the led
+    bl start_seq
+
+    pop {pc}
 
 /////////////////////////////////////////////////////////////////////////////
 
 .thumb_func
 dummy_handler:  
-b .  // do nothing
+    b .  // do nothing
 
