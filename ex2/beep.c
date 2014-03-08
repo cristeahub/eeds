821fe163e629b7f03c05efd5641a13e89fbb8470
Stian Jensen <me@stianj.com>
2014-03-08 01:05:29 +0100

    Add beep test

---
+++ ex2/beep.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include "efm32gg.h"
+
+uint8_t tone[14] = {127, 182, 226, 251, 252, 227, 184, 129, 74, 29, 3, 2, 25, 69};
+int i=0;
+
+void play_tone() {
+
+    if (i == 14) i=0;
+    *DAC0_CH0DATA = tone[i];
+    i++;
+}
