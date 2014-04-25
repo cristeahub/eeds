56159e4017237465420f42b1b82eb9b6845f3261
Stian Jensen <me@stianj.com>
2014-04-25 12:22:33 +0200

    Alloc char device

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -6,11 +6,48 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
 
 #include "efm32gg.h"
 
-static void interrupt_handler(int irq, void *dev_id, struct pt_regs *regs);
+static dev_t device_number;
+struct cdev driver_cdev;
 
+#define DRIVER_NAME "gamepad"
+
+static int __init driver_init(void);
+static int __exit driver_cleanup(void);
+static irqreturn_t interrupt_handler(int irq, void *dev_id, struct pt_regs *regs);
+
+/* user program opens the driver */
+static int driver_open (struct inode *inode , struct file *filp );
+
+/* user program closes the driver */
+static int driver_release (struct inode *inode , struct file *filp );
+
+/* user program reads from the driver */
+static ssize_t driver_read (struct file *filp , char __user *buff ,
+        size_t count , loff_t *offp );
+
+/* user program writes to the driver */
+static ssize_t driver_write (struct file *filp , const char __user *buff ,
+        size_t count , loff_t *offp );
+
+module_init(driver_init);
+module_exit(driver_cleanup);
+
+MODULE_DESCRIPTION("Driver for gamepad");
+MODULE_LICENSE("GPL");
+
+static struct file_operations driver_fops = {
+    .owner = THIS_MODULE,
+    .read = driver_read,
+    .write = driver_write,
+    .open = driver_open,
+    .release = driver_release
+};
 
 /*
  * module_init - function to insert this module into kernel space
@@ -27,7 +64,9 @@ static int __init driver_init(void)
 
     unsigned long num_ports = GPIO_IFC - GPIO_PA_BASE; //GPIO_IFC is the last used address in GPIO
 
-    err = request_mem_region(GPIO_PA_BASE, num_ports, "gamepad driver");
+    cdev_init(&driver_cdev, &driver_fops);
+
+    err = request_mem_region(GPIO_PA_BASE, num_ports, DRIVER_NAME);
     if (err) return -1;
     ioremap_nocache(GPIO_PA_BASE, num_ports);
 
@@ -38,12 +77,20 @@ static int __init driver_init(void)
     iowrite32(0xff, GPIO_IEN);
     iowrite32(0xff, GPIO_IFC);
 
-    request_irq(17, interrupt_handler, 0, "gamepad driver", NULL);
-    request_irq(18, interrupt_handler, 0, "gamepad driver", NULL);
+    request_irq(17, interrupt_handler, 0, DRIVER_NAME, NULL);
+    request_irq(18, interrupt_handler, 0, DRIVER_NAME, NULL);
 
     iowrite32(0xFF, GPIO_EXTIFALL);
     iowrite32(0x00FF, GPIO_IEN);
     iowrite32(0xFF, GPIO_IFC);
+
+    /* Dynamically allocate device numbers */
+    int result = alloc_chrdev_region(&device_number, 0, 1, DRIVER_NAME);
+
+    if (result < 0) {
+        printk(KERN_ALERT "Failed to allocate device numbers\n");
+        return -1;
+    }
 
     printk(KERN_INFO "Gamepad driver loaded.\n");
     return 0;
@@ -60,6 +107,8 @@ static void __exit driver_cleanup(void)
 {
     printk(KERN_INFO "Unloading driver module...\n");
 
+    unregister_chrdev_region(device_number, 1);
+
     // Disable interrupts
     iowrite32(0x0000, GPIO_IEN);
     free_irq(17);
@@ -72,9 +121,5 @@ static void interrupt_handler(int irq, void *dev_id, struct pt_regs *regs) {
 
 }
 
-module_init(driver_init);
-module_exit(driver_cleanup);
 
-MODULE_DESCRIPTION("Driver for gamepad");
-MODULE_LICENSE("GPL");
 
